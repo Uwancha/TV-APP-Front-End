f2d27b5af6e040353dde600e804db92b19b5535b
Uwancha <sito2alemu@gmail.com>
2024-05-20 15:15:48 +0300

    feat: Create static version of the customer pages

---
+++ app/channel/[programs]/page.tsx
@@ -0,0 +1,10 @@
+import ResponsiveProgramsPage from '@/app/HigherOrderComponents/programs/ResponsivePage';
+import { Box } from '@mui/material'
+
+export default function Programs() {
+  return (
+    <Box>
+        <ResponsiveProgramsPage />
+    </Box>
+  );
+};
---
+++ app/channel/page.tsx
@@ -0,0 +1,7 @@
+import ResponsiveChannelPage from "../HigherOrderComponents/Channels/ResponsivePage";
+
+export default function Channel ()  {
+  return (
+    <ResponsiveChannelPage />
+  )
+};
