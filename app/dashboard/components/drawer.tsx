f735eff285157def606c4b0a6eb71529b1d30986
Uwancha <sito2alemu@gmail.com>
2024-05-24 12:33:43 +0300

    style: Fix typo in dashboard mobile sidebar component

--- app/dashboard/components/drawer.tsx
+++ app/dashboard/components/drawer.tsx
@@ -14,29 +14,29 @@ export const MobileSideBar = () => {
   
   return (
     <>
-    <IconButton onClick={toggleDrawer(true)} sx={{ position: 'absolute', left: 0, top: '50%', transform: 'translateY(-50%)' }}>
+      <IconButton onClick={toggleDrawer(true)} sx={{ position: 'absolute', left: 0, top: '50%', transform: 'translateY(-50%)' }}>
         <ChevronRight />
       </IconButton>
       <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
         <Box sx={{ width: 250 }}>
           <List sx={{display:'flex', flexDirection:'column', gap:4, paddingLeft:6, marginTop: 12}}>
             <Link href={'/dashboard'} style={{display:'flex', alignItems:'center', gap:4, color:'#161c2f', textDecoration:'none'}}>
-                <Dashboard fontSize='large'/>
-                <ListItem >
-                    <ListItemText primary="Dashboard" />
-                </ListItem>
+              <Dashboard fontSize='large'/>
+              <ListItem >
+                <ListItemText primary="Dashboard" />
+              </ListItem>
             </Link>
             <Link href={'/dashboard/programs'} style={{display:'flex', alignItems:'center', gap:4, color:'#161c2f', textDecoration:'none'}}>
-                <LinkRounded fontSize='large'/>
-                <ListItem >
-                    <ListItemText primary="Program" />
-                </ListItem>
+              <LinkRounded fontSize='large'/>
+              <ListItem >
+                <ListItemText primary="Program" />
+              </ListItem>
             </Link >
-            <Link href={'/dashboard/programs'} style={{display:'flex', alignItems:'center', gap:4, color:'#161c2f', textDecoration:'none'}}>
-                <LinkRounded fontSize='large'/>
-                <ListItem >
-                    <ListItemText primary="Channel" />
-                </ListItem>
+            <Link href={'/dashboard/channels'} style={{display:'flex', alignItems:'center', gap:4, color:'#161c2f', textDecoration:'none'}}>
+              <LinkRounded fontSize='large'/>
+              <ListItem >
+                <ListItemText primary="Channel" />
+              </ListItem>
             </Link>
           </List>
         </Box>
